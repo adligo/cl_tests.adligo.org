c15c8230d3b714548277bc7dfa2501c1c2d39a5a
scott <scott@adligo.com>
2011-01-04 03:33:56 +0000

    inital command line tests (need to do a input stub reader in adig)

---
+++ src/org/adligo/cl/MainRunner.java
@@ -0,0 +1,39 @@
+package org.adligo.cl;
+
+import java.util.Map;
+
+import org.adligo.cl.args.ArgParser;
+import org.adligo.cl.view.InputDialog;
+import org.adligo.cl.view.ParsedInputDialog;
+import org.adligo.cl.view.parsers.IntegerInputParser;
+import org.adligo.i.adig.client.GRegistry;
+import org.adligo.i.adig.client.I_GInvoker;
+import org.adligo.i.log.client.LogPlatform;
+import org.adligo.j2se.util.J2SEPlatform;
+
+
+public class MainRunner {
+	
+	static {
+		try {
+			J2SEPlatform.init();
+			LogPlatform.init();
+		} catch (Exception x) {
+			x.printStackTrace();
+		}
+	}
+	public static void main(String [] args) {
+		Map<String,String> cla = ArgParser.parse(args);
+		
+		ParsedInputDialog inputDialog = new ParsedInputDialog();
+		inputDialog.setMessage("how many carrots did you want?");
+		inputDialog.setErrorMessage("I didn't understand please try a number like 22.");
+		inputDialog.setParser(new IntegerInputParser());
+		inputDialog.show();
+		
+		Integer number = (Integer) inputDialog.getParsed();
+		if (number != null) {
+			System.out.println(" you said " + number);
+		}
+	}
+}
---
+++ src/org/adligo/cl/args/ArgParserTests.java
@@ -0,0 +1,17 @@
+package org.adligo.cl.args;
+
+import java.util.Map;
+
+import org.adligo.tests.ATest;
+
+public class ArgParserTests extends ATest {
+
+	public void testArgParser() {
+		Map<String,String> cla = ArgParser.parse(
+				new String [] {"-help","-threads='123'","-mojos=0"});
+		assertEquals("", cla.get("-help"));
+		assertEquals("123", cla.get("-threads"));
+		//-mojos didn't have quotes
+		assertEquals("", cla.get("-mojos"));
+	}
+}
